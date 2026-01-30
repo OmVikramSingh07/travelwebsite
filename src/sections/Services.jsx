923229622bb000c6da70c57fe2c3b99778ce4f57
vikram singh <omvikramsingh07@gmail.com>
2026-01-30 22:52:13 +0530

    section 3

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import service1 from '../assets/service1.webp'
+import service2 from '../assets/service2.webp'
+import service3 from '../assets/service3.webp'
+import service4 from '../assets/service4.webp'
+import service5 from '../assets/service5.webp'
+import service6 from '../assets/service6.webp'
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function Services() {
+
+  useEffect(() => {
+      AOS.init({
+        duration: 800,
+        delay: 200,
+        once: false,
+      });
+    }, []);
   return (
     <div>Services</div>
   )
