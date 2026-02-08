8ff8e794787af59eba09b081933c5cfab0c115cc
vikram singh <omvikramsingh07@gmail.com>
2026-02-08 18:34:51 +0530

    up

--- src/sections/CtaOne.jsx
+++ src/sections/CtaOne.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import ctal from '../assets/cta1.webp'
+import cta1 from '../assets/cta1.webp'
 import { FaPlay } from 'react-icons/fa'
 import AOS from "aos";
 import "aos/dist/aos.css";
@@ -15,13 +15,19 @@ function CtaOne() {
       });
   }, []);
   return (
-    <div id='ctal' style={{backgroundImage: `url({${cta1})`}} className='relative w-full lg:h-125 h-auto lg:p-22 p-10 flex flex-col justify-center items-center gap-5'>
+  <div id='cta1' style={{backgroundImage: `url(${cta1})`}} className='relative w-full lg:h-125 h-auto lg:p-22 p-10 flex flex-col justify-center items-center gap-5 bg-cover bg-center'>
+      {/* black overlay */}
+      <div className=' absolute inset-0 bg-black/50'></div>
+
+      {/* content part */}
+    <div className='relative z-10 flex felx-col justify-center items-center gap-5'>  
       <h1 data-aos="zoom-in" data-aos-delay="100" className='lg:text-5xl text-3xl font-fakhwang font-semibold text-white lg:w-[60%] w-full text-center capitalize'>Lorem ipsum dolor sit amet.</h1>
       <p data-aos="zoom-in" data-aos-delay="200" className='text-white lg:w-[50%] w-full text-lg text-center'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam voluptate voluptatum adipisci eius aliquid esse eum sequi sunt atque omnis?</p>
       <div data-aos="zoom-in" data-aos-delay="300" className='bg-white lg:p-7 p-4 rounded-full'>
           <FaPlay className='text-orange-600 text-2xl'/>
       </div>
-    </div>
+    </div>  
+  </div>
   )
 }
 
