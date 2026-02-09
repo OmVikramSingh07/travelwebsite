fb3173f2fe5de6eb610ae2353fe28e373072e9f6
vikram singh <omvikramsingh07@gmail.com>
2026-02-09 18:18:05 +0530

    3

--- src/sections/Featurs.jsx
+++ src/sections/Featurs.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { features } from '../export'
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 
 function Features() {
+
+  useEffect(() => {
+      AOS.init({
+        duration: 800,
+        delay: 200,
+        once: false,
+      });
+    }, []);
   return (
-    <div>Features</div>
+    <div id='whyus' className='w-full lg:px-22 px-5 lg:pt-22 pt-20 pb-45 flex flex-col justify-between items-center gap-5'>
+      <h1 data-aos="zoom-in" data-aos-delay="100" className='lg:text-5xl text-3xl font-fakhwang font-semibold text-black text-center'>Why should you travel with us</h1>
+      <p data-aos="zoom-in" data-aos-delay="200" className='text-gray-500 text-center lg:w-[60%] w-full'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, voluptas. Suscipit expedita eos cumque excepturi?</p>
+      <div data-aos="zoom-in" data-aos-delay="300" className='w-full grid lg:grid-cols-3 grid-cols-1 justify-center items-start gap-5 mt-5'>
+        {features.map((item, index) => (
+          <div key={index} className='flex flex-col justify-center items-center gap-5 bg-gray-200
+          rounded-md lg:p-10 p-6'>
+            <span className='bg-orange-600 hover:bg-black cursor-pointer p-4 rounded-md text-white'><item.icon className='text-3xl' /></span>
+            <h1 className='text-x1 text-center font-semibold text-black'>{item.name}</h1>
+            <p className='text-gray-500 text-center'>{item.about}</p>  
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { IoMail } from 'react-icons/io5'
+import { FaPhoneAlt } from 'react-icons/fa'
+import { AiFillInstagram } from 'react-icons/ai'
+import { FaTwitter, FaLinkedin, FaFacebook } from 'react-icons/fa'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div id='contact' className='w-full bg-black lg:px-22 px-5 py-10 flex flex-col justify-center items-center gap-12'>
+      <div id='top' className='bg-[#046e8f] w-full lg:px-10 p-6 rounded-md flex lg:flex-row flex-col justify-between items-start -mt-[120px] gap-6'>
+        <div className='lg:w-[70%] w-full flex flex-col justify-between items-start gap-5'>
+          <h1 className='text-3xl font-fakhwang font-semibold text-white text-left lg:w-[60%] w-full'>Crafting unforgettable journeys, one adventure at a time</h1>
+          <hr className='lg:w-[10%] w-[20%] border-2 border-orange-600' />
+        </div>
+        <div className='lg:w-[30%] w-full flex flex-col justify-between items-start gap-5'>
+          <h1 className='text-white text-xl'>Sbscribe our Newsletter</h1>
+          <div className='flex lg:flex-row flex-col gap-3 w-full'>
+            <input type='text' placeholder='Enter your email' className='bg-white p-3 rounded-md text-black' />
+            <button className='bg-orange-600 text-white p-3 rounded-md'>Subscribe</button>
+          </div>
+        </div>
+      </div>
+      <div id='mid' className='text-white flex lg:flex-row flex-col justify-between items-start gap-10'>
+
+        {/* first box */}
+        <div className='lg:w-[40%] w-full'>
+          <h1 className='text-3xl font-fakhwang font-semibold'>GlobesPrint</h1>
+          <p className='mt-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, vel deserunt.</p>
+          <div className='flex flex-col justify-between items-start gap-2 mt-5'>
+            <div className='flex justify-start items-center gap-2'><IoMail className='text-orange-600 text-x1' /><p>hello@globesprint.com</p></div>
+            <div className='flex justify-start items-center gap-2'><FaPhoneAlt className='text-orange-600 text-x1' /><p>+91 890 1020 101</p></div>
+          </div>
+        </div>
+        {/* first box ends */}
+
+        {/*second box */}
+
+          <div className='lg:w-[20%] w-full'>
+            <h1 className='text-x1 font-medium font-fakhwang'>Quick Links</h1>
+            <ul className='flex flex-col justify-between items-start gap-3 mt-5'>
+              <li className='text-gray-300 hover:text-orange-600 cursor-pointer'>Home</li>
+              <li className='text-gray-300 hover:text-orange-600 cursor-pointer'>About</li>
+              <li className='text-gray-300 hover:text-orange-600 cursor-pointer'>Services</li>
+              <li className='text-gray-300 hover:text-orange-600 cursor-pointer'>Features</li>
+              <li className='text-gray-300 hover:text-orange-600 cursor-pointer'>Contact</li>
+            </ul>
+          </div>    
+     {/*seconds box ends */}
+      </div>
+      <div id='last'></div>
+    </div>
   )
 }
 
