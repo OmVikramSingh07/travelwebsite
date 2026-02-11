efcd62c2bd2d8b2634fac620ff7e8ba9c33db100
vikram singh <omvikramsingh07@gmail.com>
2026-02-11 18:40:30 +0530

    header

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -46,8 +46,39 @@ function Footer() {
             </ul>
           </div>    
      {/*seconds box ends */}
+
+     {/*third box */}
+
+          <div className='lg:w-[20%] w-full'>
+            <h1 className='text-x1 font-medium font-fakhwang'>Travel Service</h1>
+            <ul className='flex flex-col justify-between items-start gap-3 mt-5'>
+              <li className='text-gray-30 hover:text-orange-600 cursor-pointer'>Tour Packages</li>
+              <li className='text-gray-300 hover:text-orange-600 cursor-pointer'>Flight & Hotel Deals</li>
+              <li className='text-gray-300 hover:text-orange-600 cursor-pointer'>Travel Insurance</li>
+              <li className='text-gray-300 hover:text-orange-600 cursor-pointer'>Visa Assistance</li>
+              <li className='text-gray-300 hover:text-orange-600 cursor-pointer'>Online Booking</li>
+            </ul>
+          </div>    
+     {/*thrid box ends */}
+
+
+          {/*fourth box */}
+
+          <div className='lg:w-[20%] w-full'>
+            <h1 className='text-x1 font-medium font-fakhwang'>Follow Us</h1>
+            <ul className='flex flex-col justify-between items-start gap-3 mt-5'>
+              <li className='text-gray-300 hover:text-orange-600 cursor-pointer flex justify-between items-center gap-2'><FaFacebook className='text-orange-600 text-x1' />Facebook</li>
+              <li className='text-gray-300 hover:text-orange-600 cursor-pointer flex justify-between items-center gap-2'><AiFillInstagram className='text-orange-600 text-x1' />Instagram</li>
+              <li className='text-gray-300 hover:text-orange-600 cursor-pointer flex justify-between items-center gap-2'><FaLinkedin className='text-orange-600 text-x1' />Linkedin</li>
+              <li className='text-gray-300 hover:text-orange-600 cursor-pointer flex justify-between items-center gap-2'><FaTwitter className='text-orange-600 text-x1' />Twitter</li>
+            </ul>
+          </div>    
+     {/*fourth box ends */}
       </div>
-      <div id='last'></div>
+      <hr className='w-full border-1 border-gray-800' />
+      <div id='last' className='flex justify-between items-center gap-2'>
+        <p className='text-gray-300 text-center'>Copyright 2026 GlobesPrint, All Rights Reserved.</p>
+      </div>
     </div>
   )
 }
