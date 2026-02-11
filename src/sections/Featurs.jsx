1d3df8c0949b3e706f90c2fa43cd596d356bac1e
vikram singh <omvikramsingh07@gmail.com>
2026-02-11 19:18:12 +0530

    features

--- src/sections/Featurs.jsx
+++ src/sections/Featurs.jsx
@@ -16,7 +16,8 @@ function Features() {
   return (
     <div id='whyus' className='w-full lg:px-22 px-5 lg:pt-22 pt-20 pb-45 flex flex-col justify-between items-center gap-5'>
       <h1 data-aos="zoom-in" data-aos-delay="100" className='lg:text-5xl text-3xl font-fakhwang font-semibold text-black text-center'>Why should you travel with us</h1>
-      <p data-aos="zoom-in" data-aos-delay="200" className='text-gray-500 text-center lg:w-[60%] w-full'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, voluptas. Suscipit expedita eos cumque excepturi?</p>
+      <p data-aos="zoom-in" data-aos-delay="200" className='text-gray-500 text-center lg:w-[60%] w-full'>We don’t just plan trips — we create experiences. Our team works closely with you to understand your preferences and design a journey that matches your expectations.
+      With reliable partnerships, expert guidance, and 24/7 assistance, traveling with GlobeSprint means traveling with confidence.</p>
       <div data-aos="zoom-in" data-aos-delay="300" className='w-full grid lg:grid-cols-3 grid-cols-1 justify-center items-start gap-5 mt-5'>
         {features.map((item, index) => (
           <div key={index} className='flex flex-col justify-center items-center gap-5 bg-gray-200
